f31bf2345c135f7d6cbeed5757cf9d6372067518
gentiusb <genti_mitro@outlook.com>
2022-11-27 21:43:08 +0100

    title bar removed and, the color of status bar and app's logo changed

--- app/src/main/java/com/example/aesencryptiontool/MainActivity.java
+++ app/src/main/java/com/example/aesencryptiontool/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.aesencryptiontool;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.background));
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//
+        }
         setContentView(R.layout.activity_main);
     }
 }
